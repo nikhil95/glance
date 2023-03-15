306a73edcc78a494ea9ca16b7d3c041698856468
ranjannikhil <ranjannikhil21@gmail.com>
2023-03-15 19:24:14 +0530

    first commit

---
+++ node_modules/@girder/components/src/components/Authentication/index.js
@@ -0,0 +1,13 @@
+import GirderAuthentication from './Authentication.vue';
+import GirderLogin from './Login.vue';
+import GirderOAuth from './OAuth.vue';
+import GirderRegister from './Register.vue';
+
+export {
+  GirderAuthentication,
+  GirderLogin,
+  GirderOAuth,
+  GirderRegister,
+};
+
+export default GirderAuthentication;
